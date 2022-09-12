1e9784624c0990b2ad100793acadfb8d27716178
goldie chukwuedo <goldiechukwuedoo@gmail.com>
2022-09-12 02:13:13 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - returns 4
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
